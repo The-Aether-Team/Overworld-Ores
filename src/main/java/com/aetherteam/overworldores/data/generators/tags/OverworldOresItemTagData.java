6ef8785b6559a21afba3162aba5d8487c07e1237
bconlon <bconlon.gml@gmail.com>
2025-09-02 20:44:37 -0700

    feat: add holystone ores to ore item tags

--- src/main/java/com/aetherteam/overworldores/data/generators/tags/OverworldOresItemTagData.java
+++ src/main/java/com/aetherteam/overworldores/data/generators/tags/OverworldOresItemTagData.java
@@ -8,6 +8,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
@@ -23,6 +24,15 @@ public class OverworldOresItemTagData extends ItemTagsProvider {
 
     @Override
     public void addTags(HolderLookup.Provider provider) {
+        this.tag(ItemTags.COAL_ORES).add(OverworldOresBlocks.HOLYSTONE_COAL_ORE.get().asItem());
+        this.tag(ItemTags.IRON_ORES).add(OverworldOresBlocks.HOLYSTONE_IRON_ORE.get().asItem());
+        this.tag(ItemTags.COPPER_ORES).add(OverworldOresBlocks.HOLYSTONE_COPPER_ORE.get().asItem());
+        this.tag(ItemTags.GOLD_ORES).add(OverworldOresBlocks.HOLYSTONE_GOLD_ORE.get().asItem());
+        this.tag(ItemTags.REDSTONE_ORES).add(OverworldOresBlocks.HOLYSTONE_REDSTONE_ORE.get().asItem());
+        this.tag(ItemTags.LAPIS_ORES).add(OverworldOresBlocks.HOLYSTONE_LAPIS_ORE.get().asItem());
+        this.tag(ItemTags.EMERALD_ORES).add(OverworldOresBlocks.HOLYSTONE_EMERALD_ORE.get().asItem());
+        this.tag(ItemTags.DIAMOND_ORES).add(OverworldOresBlocks.HOLYSTONE_DIAMOND_ORE.get().asItem());
+
         for (ModdedOres.OreKey ore : ModdedOres.ORE_MOD_MAP.keySet()) {
             this.tag(Tags.Items.ORES).add(ore.holystoneOreBlock().get().asItem());
             this.tag(TagKey.create(Registries.ITEM, new ResourceLocation("forge", "ores/" + ore.name()))).add(ore.holystoneOreBlock().get().asItem());
